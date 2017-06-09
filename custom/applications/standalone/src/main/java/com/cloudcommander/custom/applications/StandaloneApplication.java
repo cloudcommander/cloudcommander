aa4dd69811ce1c1b0d2e278404b93e18f70ff135
Adrian Tello <adriantl7@gmail.com>
2017-06-09 19:43:51 +0200

    Create hello world application

---
+++ custom/applications/standalone/src/main/java/com/cloudcommander/custom/applications/StandaloneApplication.java
@@ -0,0 +1,10 @@
+package com.cloudcommander.custom.applications;
+
+/**
+ * Created by Adrian Tello on 09/06/2017.
+ */
+public class StandaloneApplication {
+    public static void main (String [ ] args) {
+        System.out.println("Hello world");
+    }
+}
