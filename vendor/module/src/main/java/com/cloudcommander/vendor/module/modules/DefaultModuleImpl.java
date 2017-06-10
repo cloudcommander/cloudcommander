196bd8377f2ff83798cdcdad2823fd8be127b701
Adrian Tello <adriantl7@gmail.com>
2017-06-10 13:56:41 +0200

    Add the springConfigClass property to the Module objects

--- vendor/module/src/main/java/com/cloudcommander/vendor/module/modules/DefaultModuleImpl.java
+++ vendor/module/src/main/java/com/cloudcommander/vendor/module/modules/DefaultModuleImpl.java
@@ -1,6 +1,7 @@
 package com.cloudcommander.vendor.module.modules;
 
 import java.util.Collection;
+import java.util.Optional;
 
 /**
  * Created by Adrian Tello on 09/06/2017.
@@ -11,9 +12,18 @@ public class DefaultModuleImpl implements Module{
 
     private Collection<String> requiredModuleNames;
 
+    private final Class springConfigClass;
+
     public DefaultModuleImpl(final String name, Collection<String> requiredModuleNames) {
         this.name = name;
         this.requiredModuleNames = requiredModuleNames;
+        this.springConfigClass = null;
+    }
+
+    public DefaultModuleImpl(final String name, Collection<String> requiredModuleNames, Class sprintConfigClass) {
+        this.name = name;
+        this.requiredModuleNames = requiredModuleNames;
+        this.springConfigClass = sprintConfigClass;
     }
 
     @Override
@@ -24,5 +34,10 @@ public class DefaultModuleImpl implements Module{
     @Override
     public Collection<String> getRequiredModuleNames() {
         return requiredModuleNames;
+    }
+
+    @Override
+    public Optional<Class> getSpringConfigClass() {
+        return Optional.ofNullable(springConfigClass);
     }
 }
--- vendor/module/src/main/java/com/cloudcommander/vendor/module/modules/Module.java
+++ vendor/module/src/main/java/com/cloudcommander/vendor/module/modules/Module.java
@@ -1,6 +1,7 @@
 package com.cloudcommander.vendor.module.modules;
 
 import java.util.Collection;
+import java.util.Optional;
 
 /**
  * Created by Adrian Tello on 09/06/2017.
@@ -9,4 +10,6 @@ public interface Module {
     String getName();
 
     Collection<String> getRequiredModuleNames();
+
+    Optional<Class> getSpringConfigClass();
 }
