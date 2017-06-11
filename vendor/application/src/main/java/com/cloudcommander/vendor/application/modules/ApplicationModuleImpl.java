910377157aeee0ccfb2a1a3e5b52b1ff90e09852
Adrian Tello <adriantl7@gmail.com>
2017-06-11 20:32:38 +0200

    Configure the application module as a module

---
+++ vendor/application/src/main/java/com/cloudcommander/vendor/application/modules/ApplicationModuleImpl.java
@@ -0,0 +1,20 @@
+package com.cloudcommander.vendor.application.modules;
+
+import com.cloudcommander.vendor.module.modules.DefaultModuleImpl;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Created by Adrian Tello on 11/06/2017.
+ */
+public class ApplicationModuleImpl extends DefaultModuleImpl{
+
+    private final static String MODULE_NAME = "application";
+
+    private final static Collection<String> REQUIRED_MODULES = Arrays.asList("module");
+
+    public ApplicationModuleImpl() {
+        super(MODULE_NAME, REQUIRED_MODULES);
+    }
+}
