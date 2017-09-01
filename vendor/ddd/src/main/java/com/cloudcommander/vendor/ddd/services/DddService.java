fa00d9025a4b5f74625571bb77391b9e59dcec24
Adrian Tello <adriantl7@gmail.com>
2017-09-02 00:28:15 +0200

    Create the dispatch methods in the DddService

--- vendor/ddd/src/main/java/com/cloudcommander/vendor/ddd/services/DddService.java
+++ vendor/ddd/src/main/java/com/cloudcommander/vendor/ddd/services/DddService.java
@@ -1,5 +1,15 @@
 package com.cloudcommander.vendor.ddd.services;
 
+import akka.util.Timeout;
+import com.cloudcommander.vendor.ddd.aggregates.commands.Command;
+
+import java.util.concurrent.CompletionStage;
+
 public interface DddService {
 
+    void dispatchAndForget(Command command);
+
+    CompletionStage<Object> dispatch(Command command);
+
+    CompletionStage<Object> dispatch(Command command, Timeout timeout);
 }
--- vendor/ddd/src/main/java/com/cloudcommander/vendor/ddd/services/DefaultDddService.java
+++ vendor/ddd/src/main/java/com/cloudcommander/vendor/ddd/services/DefaultDddService.java
@@ -6,14 +6,27 @@ import akka.actor.Props;
 import akka.cluster.sharding.ClusterSharding;
 import akka.cluster.sharding.ClusterShardingSettings;
 import akka.cluster.sharding.ShardRegion;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
 import com.cloudcommander.vendor.ddd.aggregates.AggregateDefinition;
+import com.cloudcommander.vendor.ddd.aggregates.commands.Command;
 import com.cloudcommander.vendor.ddd.akka.actors.AggregateActor;
+import scala.compat.java8.FutureConverters;
+import scala.concurrent.Future;
+import java.util.concurrent.CompletionStage;
 
 public class DefaultDddService implements DddService{
 
+    private Timeout timeout;
+
+    private ActorSystem actorSystem;
+
     private ActorRef aggregateRegion;
 
-    public DefaultDddService(ActorSystem actorSystem, ShardRegion.MessageExtractor shardMessageExtractor, AggregateDefinition aggregateDefinition) {
+    public DefaultDddService(ActorSystem actorSystem, ShardRegion.MessageExtractor shardMessageExtractor, Timeout timeout, AggregateDefinition aggregateDefinition) {
+        this.timeout = timeout;
+        this.actorSystem = actorSystem;
+
         String name = aggregateDefinition.getName();
         String boundedCtxName = aggregateDefinition.getBoundedContextDefinition().getName();
         String regionName = boundedCtxName + "--" + name;
@@ -25,8 +38,33 @@ public class DefaultDddService implements DddService{
 
     }
 
+    @Override
+    public void dispatchAndForget(Command command){
+        aggregateRegion.tell(command, ActorRef.noSender());
+    }
+
+    @Override
+    public CompletionStage<Object> dispatch(Command command){
+        return dispatch(command, timeout);
+    }
+
+    @Override
+    public CompletionStage<Object> dispatch(Command command, Timeout timeout) {
+        Future<Object> future = Patterns.ask(aggregateRegion, command, timeout);
+
+        return FutureConverters.toJava(future);
+    }
+
     protected ActorRef getAggregateRegion() {
         return aggregateRegion;
+    }
+
+    protected ActorSystem getActorSystem() {
+        return actorSystem;
+    }
+
+    public Timeout getTimeout() {
+        return timeout;
     }
 }
 
