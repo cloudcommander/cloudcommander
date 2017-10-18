0e0e5457beb7ebc7b187cc38616f6b8ca9ab7ba7
Adrian Tello <adriantl7@gmail.com>
2017-10-18 20:46:40 +0200

    Remove all managers code (until the aggregates codes is ready)

--- vendor/ddd/src/main/java/com/cloudcommander/vendor/ddd/managers/managerevents/ManagerEvent.java
+++
@@ -1,9 +0,0 @@
-package com.cloudcommander.vendor.ddd.managers.managerevents;
-
-import java.io.Serializable;
-
-/**
- * Created by Adrian Tello on 16/10/2017.
- */
-public interface ManagerEvent extends Serializable{
-}
