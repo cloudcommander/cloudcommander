235d041433b4655a2621503ca5b9e7a066b5aa19
Adrian Tello <adriantl7@gmail.com>
2017-11-03 15:01:31 +0100

    Correct camelcase name for ProtoUuidConverters

---
+++ vendor/ddd/src/test/java/com/cloudcommander/vendor/ddd/entities/akka/actors/counter/serialization/converters/commands/ProtoIncrementCommandConverter.java
@@ -0,0 +1,7 @@
+package com.cloudcommander.vendor.ddd.entities.akka.actors.counter.serialization.converters.commands;
+
+/**
+ * Created by adria on 03/11/2017.
+ */
+public interface ProtoIncrementCommandConverter {
+}
